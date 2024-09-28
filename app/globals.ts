eab82384a233b09ccaf5ea12db6a0360259902de
markoblagdan <mblagdan@gmail.com>
2024-09-29 00:36:51 +0200

    turn off instrumentation and data seeding

--- app/globals.ts
+++ app/globals.ts
@@ -3,3 +3,7 @@ import { SynonymsRepository } from "./_common/_repositories/synonyms-repository"
 export const globalRef = global as typeof globalThis & {
   synonymsRepository: SynonymsRepository;
 };
+
+if (!globalRef.synonymsRepository) {
+  globalRef.synonymsRepository = new SynonymsRepository();
+}
