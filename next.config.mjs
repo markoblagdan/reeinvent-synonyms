35bc6d1af7c1fe1956b688f07bb22a2c0bccf7b2
markoblagdan <mblagdan@gmail.com>
2024-09-29 01:23:55 +0200

    config for loading data file

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     instrumentationHook: true,
   },
+  outputFileTracingIncludes: {
+    "app/_common/_repositories/synonyms-repository": ["./data/synonyms.csv"],
+  },
 };
 
 export default nextConfig;
